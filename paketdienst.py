9830441279ae2fc16af7b0080455fb9572fc99bc
ChrisHaSaar <christian.hammenstede@gmail.com>
2023-11-15 11:49:29 +0100

    Neue Funktion: sollte eine neue Version von pip vorhanden sein, wird pip aktualisiert, dann wird geprüft-

--- paketdienst.py
+++ paketdienst.py
@@ -1,9 +1,38 @@
+"""
+Python Paketdienst
+
+Es wird geprüft, ob pip aktuell ist, falls nicht, wird es direkt geupdated.
+Danach werden alle installierten Pakete gecheckt und notfalls erneuert.
+In dem Verzeichnis Log-Files werden die entsprechenden Log-Dateien angelegt.
+damit man weiß, was genau passiert ist.
+
+geschrieben von:
+Christian Hammenstede, 15.11.2023
+christian.hammenstede@gmail.com
+
+"""
+
 import subprocess
+import os
 from datetime import datetime
 from importlib import metadata
 
-# Log-Datei erstellen
-log_file = f"pip_upgrade_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+def upgrade_pip():
+    """ Führt ein Upgrade von pip durch, falls erforderlich. """
+    result = subprocess.run("pip list --outdated", shell=True, capture_output=True, text=True)
+    if 'pip' in result.stdout:
+        print("Aktualisiere pip...")
+        subprocess.run("python -m pip install --upgrade pip", shell=True)
+
+# Pip zuerst aktualisieren
+upgrade_pip()
+
+# Pfad für den Log-Ordner festlegen und erstellen, falls nicht vorhanden
+log_folder = "Log-Files"
+os.makedirs(log_folder, exist_ok=True)
+
+# Log-Datei-Pfad erstellen
+log_file = os.path.join(log_folder, f"pip_upgrade_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt")
 
 with open(log_file, "w") as log:
     # Liste aller installierten Pakete erhalten
