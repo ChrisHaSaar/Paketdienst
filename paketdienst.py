abb24bea64c04a87104646ea82d0dc3b31c2464c
ChrisHaSaar <christian.hammenstede@gmail.com>
2023-11-15 12:13:56 +0100

    Konfigurationsdatei angelegt Mit diesem Skript werden Pakete basierend auf der Konfigurationsdatei entweder automatisch oder manuell aktualisiert. Stellen Sie sicher, dass die Datei: update_config.json im selben Verzeichnis wie Ihr Skript liegt oder passen Sie den Pfad entsprechend an.
    Beispiel für den Inhalt der Konf-Datei:
    
    {
        "auto_update": ["numpy", "pandas"],
        "manual_update": ["django", "flask"]
    }
    
    In diesem Beispiel werden numpy und pandas automatisch aktualisiert, während django und flask manuelle Bestätigung erfordern.

--- paketdienst.py
+++ paketdienst.py
@@ -14,6 +14,7 @@ christian.hammenstede@gmail.com
 
 import subprocess
 import os
+import json
 from datetime import datetime
 from importlib import metadata
 
@@ -28,8 +29,11 @@ def dry_run_upgrade(package):
     result = subprocess.run(f"pip install --upgrade --dry-run {package}", shell=True, capture_output=True, text=True)
     return result.stdout
 
-# Pip zuerst aktualisieren
-upgrade_pip()
+# Konfigurationsdatei lesen
+with open('update_config.json') as config_file:
+    config = json.load(config_file)
+auto_update_packages = config.get("auto_update", [])
+manual_update_packages = config.get("manual_update", [])
 
 # Pfad für den Log-Ordner festlegen und erstellen, falls nicht vorhanden
 log_folder = "Log-Files"
@@ -38,30 +42,23 @@ os.makedirs(log_folder, exist_ok=True)
 # Log-Datei-Pfad erstellen
 log_file = os.path.join(log_folder, f"pip_upgrade_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt")
 
-# Dry Run für alle Pakete durchführen und Ergebnisse sammeln
-upgrade_candidates = []
-for dist in metadata.distributions():
-    package = dist.metadata['Name']
-    dry_run_result = dry_run_upgrade(package)
-    if dry_run_result.strip():
-        upgrade_candidates.append(package)
-        print(f"Vorgeschlagene Aktualisierung für {package}:\n{dry_run_result}")
+# Pip zuerst aktualisieren
+upgrade_pip()
 
-# Benutzer um Entscheidung bitten
-if upgrade_candidates:
-    decision = input("Möchten Sie alle vorgeschlagenen Aktualisierungen durchführen? [y/n]: ").strip().lower()
-    if decision == 'y':
-        with open(log_file, "w") as log:
-            for package in upgrade_candidates:
-                log.write(f"Aktualisiere {package}\n")
-                update_result = subprocess.run(f"pip install --upgrade {package}", shell=True, capture_output=True, text=True)
-                log.write(update_result.stdout)
-                if update_result.stderr:
-                    log.write(f"Fehler bei der Aktualisierung von {package}:\n{update_result.stderr}\n")
-                print(f"{package} wurde aktualisiert.")
-    else:
-        print("Aktualisierung abgebrochen.")
-else:
-    print("Keine Aktualisierungen notwendig.")
+with open(log_file, "w") as log:
+    # Jedes Paket überprüfen und ggf. aktualisieren
+    for dist in metadata.distributions():
+        package = dist.metadata['Name']
+        if package in auto_update_packages:
+            # Automatische Aktualisierung
+            log.write(f"Aktualisiere {package} automatisch\n")
+            subprocess.run(f"pip install --upgrade {package}", shell=True)
+        elif package in manual_update_packages:
+            # Manuelle Aktualisierung mit Bestätigung
+            dry_run_result = dry_run_upgrade(package)
+            decision = input(f"Möchten Sie {package} aktualisieren? [y/n]: ").strip().lower()
+            if decision == 'y':
+                log.write(f"Aktualisiere {package} manuell\n")
+                subprocess.run(f"pip install --upgrade {package}", shell=True)
 
 print(f"Aktualisierung abgeschlossen. Log-Datei gespeichert unter: {log_file}")
